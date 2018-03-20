b5bac3ee968f130032a73a52a458c0fe75cf3714
nicolas.dorier <nicolas.dorier@gmail.com>
2018-03-21 01:04:44 +0900

    Add EXPOSE_TCP turned off by default

--- tools/docker-entrypoint.sh
+++ tools/docker-entrypoint.sh
@@ -4,12 +4,19 @@ cat <<-EOF > "$LIGHTNINGD_DATA/config"
 ${LIGHTNINGD_OPT}
 EOF
 
-lightningd &
+: ${EXPOSE_TCP:=false}
 
-echo "C-Lightning starting"
-while read i; do if [ "$i" = "lightning-rpc" ]; then break; fi; done \
-   < <(inotifywait  -e create,open --format '%f' --quiet $LIGHTNINGD_DATA --monitor)
-echo "C-Lightning started"
+if [ "$EXPOSE_TCP" == "true" ]; then
+
+    lightningd &
+
+    echo "C-Lightning starting"
+    while read i; do if [ "$i" = "lightning-rpc" ]; then break; fi; done \
+    < <(inotifywait  -e create,open --format '%f' --quiet $LIGHTNINGD_DATA --monitor)
+    echo "C-Lightning started"
 
 
-socat -d -d TCP4-listen:$LIGHTNINGD_PORT,fork,reuseaddr UNIX-CONNECT:$LIGHTNINGD_DATA/lightning-rpc
+    socat -d -d TCP4-listen:$LIGHTNINGD_PORT,fork,reuseaddr UNIX-CONNECT:$LIGHTNINGD_DATA/lightning-rpc
+else
+    lightningd
+fi
