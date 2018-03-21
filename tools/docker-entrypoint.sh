07686a354ddd127f10b68242d5a893d215474d36
nicolas.dorier <nicolas.dorier@gmail.com>
2018-03-21 12:31:06 +0900

    All easier way to pass network parameters to CLightning

--- tools/docker-entrypoint.sh
+++ tools/docker-entrypoint.sh
@@ -6,6 +6,37 @@ EOF
 
 : ${EXPOSE_TCP:=false}
 
+export NETWORK=`cat $LIGHTNINGD_DATA/config | sed -n 's/^network=\(.*\)$/\1/p'`
+export CHAIN=`cat $LIGHTNINGD_DATA/config | sed -n 's/^chain=\(.*\)$/\1/p'`
+
+sed -i '/^chain=/d' $LIGHTNINGD_DATA/config
+
+export REPLACEDNETWORK="";
+if [ "$CHAIN" == "btc" ]; then
+    if [ "$NETWORK" == "mainnet" ]; then
+        REPLACEDNETWORK="bitcoin"
+    fi
+fi
+
+if [ "$CHAIN" == "ltc" ]; then
+    if [ "$NETWORK" == "mainnet" ]; then
+        REPLACEDNETWORK="litecoin"
+    fi
+    if [ "$NETWORK" == "testnet" ]; then
+        REPLACEDNETWORK="litecoin-testnet"
+    fi
+    if [ "$NETWORK" == "regtest" ]; then
+        echo "REGTEST NOT AVAILABLE FOR LTC"
+        exit
+    fi
+fi
+
+if [[ $REPLACEDNETWORK ]]; then
+    sed -i '/^network=/d' $LIGHTNINGD_DATA/config
+    echo "network=$REPLACEDNETWORK" >> $LIGHTNINGD_DATA/config
+    echo "Replaced network $NETWORK by $REPLACEDNETWORK in $LIGHTNINGD_DATA/config"
+fi
+
 if [ "$EXPOSE_TCP" == "true" ]; then
 
     lightningd &
